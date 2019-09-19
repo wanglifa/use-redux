27a35ffb7ccf0523a5978c16753a9728c92ac939
wanglifa <wanglifa1995@gmail.com>
2019-09-19 15:02:44 +0800

    使用React+Redux实现counter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  add1 = () => {
+    this.props.store.dispatch({ type: 'add', payload: 1 })
+  }
+  minus1 = () => {
+    this.props.store.dispatch({ type: 'add', payload: -1 })
+  }
+  addIfOdd = () => {
+    if (this.props.store.getState() % 2 === 1) {
+      this.props.store.dispatch({ type: 'add', payload: 1 })
+    }
+  }
+  addSync = () => {
+    setTimeout(() => {
+      this.props.store.dispatch({ type: 'add', payload: 1 })
+    }, 2000)
+  }
+  render() {
+    return (
+      <div className="App">
+         <div>
+        你点击了<span id="value">{this.props.store.getState()}</span>次
+        <div>
+          <button id="add1" onClick={this.add1}>+1</button>
+          <button id="minus1" onClick={this.minus1}>-1</button>
+          <button id="addIfOdd" onClick={this.addIfOdd}>如果是单数就+1</button>
+          <button id="add1After2Sec" onClick={this.addSync}>两秒后+1</button>
+        </div>
+      </div>
+      </div>
+    )
+  }
+  
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+const stateChange = (state = 0, action) => {
+    if (action.type === "add") {
+        let newState = state + action.payload;
+        return newState;
+    } else {
+        return state;
+    }
+}
+const store = createStore(stateChange)
+function render() {
+    ReactDOM.render(
+        <App store={store}/>, 
+        document.getElementById('root')
+    );
+}
+render()
+store.subscribe(render)
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
